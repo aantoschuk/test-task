4bddfd7d224d18a83822181cf47057f444bf74f0
Alexander <alexander.antoschuk.dev@gmail.com>
2023-02-20 20:34:48 +0200

    add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
+import "./styles/app.css";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <main>
       <h1>Users</h1>
-    </>
+      <section className="user-list-section">
+        <div className="users-container">
+          <p>Hello</p>
+        </div>
+      </section>
+    </main>
   );
 }
 
